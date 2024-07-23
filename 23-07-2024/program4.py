8fd30672ed684ece2f0b544eec7c826d23278354
ADINESHKARTHICK <dinesha1406@gmail.com>
2024-07-23 08:38:10 -0700

    Delete 23-07-2024 directory

--- 23-07-2024/program4.py
+++
@@ -1,8 +0,0 @@
-def add(a, b):
-    while b != 0:
-        carry = a & b
-        a = a ^ b
-        b = carry << 1
-    return a
-result = add(num1, num2)
-print(f"The sum of {num1} and {num2} is {result}.")
